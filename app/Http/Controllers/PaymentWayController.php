7416241a8012129ff16ed1ad047c218d55ae01e4
s_fujioka <espoir519@yahoo.co.jp>
2024-05-25 09:51:48 +0900

    支払方法設定のファイル作成

---
+++ app/Http/Controllers/PaymentWayController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePaymentWayRequest;
+use App\Http\Requests\UpdatePaymentWayRequest;
+use App\Models\PaymentWay;
+
+class PaymentWayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePaymentWayRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PaymentWay $paymentWay)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PaymentWay $paymentWay)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePaymentWayRequest $request, PaymentWay $paymentWay)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PaymentWay $paymentWay)
+    {
+        //
+    }
+}
