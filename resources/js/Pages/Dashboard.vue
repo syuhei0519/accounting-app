d29601e094a33a940d73eeee06ba902bd08c27b5
s_fujioka <espoir519@yahoo.co.jp>
2024-05-29 01:16:43 +0900

    ダッシュボードのデザインを修正

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,36 +13,44 @@ import { Link } from '@inertiajs/vue3';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-2 gap-6">
+                <div class="group bg-red-100 overflow-hidden shadow-sm sm:rounded-lg p-8 hover:bg-red-200 relative" style="height: 250px;">
+                    <Link :href="`/income`" class="text-blue-500 hover:text-blue-800 font-semibold block w-full h-full">収入一覧</Link>
+                    <div class="absolute bg-white p-3 rounded-lg shadow-lg text-gray-900 text-sm hidden group-hover:block font-semibold" style="bottom: 10px; right: 10px;">
+                        登録した収入が確認できます。収入の編集、登録もできます
+                    </div>
                 </div>
-            </div>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <Link :href="`/income`" class="text-blue-500 hover:text-blue-800">収入一覧</Link>
+                <div class="group bg-orange-100 overflow-hidden shadow-sm sm:rounded-lg p-8 hover:bg-orange-200 relative" style="height: 250px;">
+                    <Link :href="`/spend`" class="text-blue-500 hover:text-blue-800 font-semibold block w-full h-full">支出一覧</Link>
+                    <div class="absolute bg-white p-3 rounded-lg shadow-lg text-gray-900 text-sm hidden group-hover:block font-semibold" style="bottom: 10px; right: 10px;">
+                        登録した支出が確認できます。支出の編集、登録もできます
+                    </div>
                 </div>
-            </div>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <Link :href="`/spend`" class="text-blue-500 hover:text-blue-800">支出一覧</Link>
+                <div class="group bg-red-100 overflow-hidden shadow-sm sm:rounded-lg p-8 hover:bg-red-200 relative" style="height: 250px;">
+                    <Link :href="`/incomeCategory`" class="text-blue-500 hover:text-blue-800 font-semibold block w-full h-full">収入カテゴリー一覧</Link>
+                    <div class="absolute bg-white p-3 rounded-lg shadow-lg text-gray-900 text-sm hidden group-hover:block font-semibold" style="bottom: 10px; right: 10px;">
+                        登録した収入カテゴリーが確認できます。編集、登録もできます
+                    </div>
                 </div>
-            </div>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <Link :href="`/incomeCategory`" class="text-blue-500 hover:text-blue-800">収入カテゴリー一覧</Link>
+                <div class="group bg-orange-100 overflow-hidden shadow-sm sm:rounded-lg p-8 hover:bg-orange-200 relative" style="height: 250px;">
+                    <Link :href="`/spendCategory`" class="text-blue-500 hover:text-blue-800 font-semibold block w-full h-full">支出カテゴリー一覧</Link>
+                    <div class="absolute bg-white p-3 rounded-lg shadow-lg text-gray-900 text-sm hidden group-hover:block font-semibold" style="bottom: 10px; right: 10px;">
+                        登録した支出カテゴリーが確認できます。編集、登録もできます
+                    </div>
                 </div>
-            </div>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <Link :href="`/spendCategory`" class="text-blue-500 hover:text-blue-800">支出カテゴリー一覧</Link>
-                </div>
-            </div>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <Link :href="`/paymentWay`" class="text-blue-500 hover:text-blue-800">決済方法一覧</Link>
+                <div class="group bg-red-100 overflow-hidden shadow-sm sm:rounded-lg p-8 hover:bg-red-200 relative" style="height: 250px;">
+                    <Link :href="`/paymentWay`" class="text-blue-500 hover:text-blue-800 font-semibold block w-full h-full">決済方法一覧</Link>
+                    <div class="absolute bg-white p-3 rounded-lg shadow-lg text-gray-900 text-sm hidden group-hover:block font-semibold" style="bottom: 10px; right: 10px;">
+                        登録した決済方法が確認できます。編集、登録もできます
+                    </div>
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<style scoped>
+.group-hover:block {
+    display: block;
+}
+</style>
